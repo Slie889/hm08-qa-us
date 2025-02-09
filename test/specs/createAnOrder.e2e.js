9f6328f2eb72fbc9b8858f79334d4d1bb9d31374
Slie889 <suv99@live.com>
2025-02-09 00:11:43 -0800

    Update createAnOrder.e2e.js

--- test/specs/createAnOrder.e2e.js
+++ test/specs/createAnOrder.e2e.js
@@ -6,59 +6,21 @@ describe('Create an order', () => {
         await browser.url(`/`)
         await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
 
-        const paymentMethodButton = await $(page.paymentMethodButton);
-        await paymentMethodButton.waitForDisplayed();
-        await paymentMethodButton.click();
-
-        const addCardButton = await $(page.addCardButton);
-        await addCardButton.waitForDisplayed();
-        await addCardButton.click();
-
-        const cardNumber = await $(page.cardNumber);
-        await cardNumber.waitForDisplayed();
-        await cardNumber.setValue(1234567812345678);
-        const cardCode = await $(page.cardCode);
-        await cardCode.waitForDisplayed();
-        await cardCode.setValue(55);
-
-        const cardSignatureStrip = await $(page.cardSignatureStrip);
-        await cardSignatureStrip.waitForDisplayed();
-        await cardSignatureStrip.click();
-
-             
-        const linkCardButton = await $(page.linkCardButton);
-        await linkCardButton.waitForDisplayed();
-        await linkCardButton.click();
-
-
-        const closePaymentMethodModalButton = await $(page.closePaymentMethodModalButton);
-        await closePaymentMethodModalButton.waitForDisplayed();
-        await closedPaymentMethodModalButton.click();
+        //Adding a payment card
+        await page.addPaymentMethodCard();
 
         const cardPaymentMethodIcon = await $(page.cardPaymentMethodIcon);
         await cardPaymentMethodIcon.waitForDisplayed();
         await expect(await $(cardPaymentMethodIcon)).toBeExisting();
 
-        await browser.pause(2000);
-    
+        await browser.pause(2000);   
+
+        //Input phone number
         const phoneNumber = helper.getPhoneNumber("+1");
         await page.submitPhoneNumber(phoneNumber);
         await expect(await helper.getElementByText(phoneNumber)).toBeExisting);
-        
-        
-        const phoneNumberButton = await $(page.phoneNumberButton);
-        await phoneNumberButton.waitForDisplayed();
-        await phoneNumberButton.click();
-        const pnoneNumberModal = await $(page.phoneNumberModal);
-        await expect(pnoneNumberModal).toBeExisting();
     })
-
-    it('should save the phone', async () => {
-        await browser.url(`/`)
-        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
-        const phoneNumber = helper.getPhoneNumber("+1");
-        await page.submitPhoneNumber(phoneNumber);
-        await expect(await helper.getElementByText(phoneNumber)).toBeExisting();
-    })
+     
+      
 })
 
