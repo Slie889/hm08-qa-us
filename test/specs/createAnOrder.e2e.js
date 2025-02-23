381e15e8c0a62f9016cd3ff2bfce92aea79b45b0
Slie889 <suv99@live.com>
2025-02-22 21:30:59 -0800

    Add files via upload

--- test/specs/createAnOrder.e2e.js
+++ test/specs/createAnOrder.e2e.js
@@ -2,25 +2,61 @@ const page = require('../../page');
 const helper = require('../../helper')
 
 describe('Create an order', () => {
-    it('should open phone number modal', async () => {
-        await browser.url(`/`)
+    it('should wait for the taxi driver', async () => {
+        //Call the taxi to the address
+        await browser.url (`https://cnt-0b6dbff6-b294-45f1-8713-bf54bbacba84.containerhub.tripleten-services.com/`);
         await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
 
-        //Adding a payment card
-        await page.addPaymentMethodCard();
+       //Adding a payment card
+        const paymentMethodButton = await $(page.paymentMethodButton);
+        await paymentMethodButton.waitForDisplayed();
+        await paymentMethodButton.click();
 
+        const addCardButton = await $(page.addCardButton);
+        await addCardButton.waitForDisplayed();
+        await addCardButton.click();
+        const cardNumber = await $(page.cardNumber);
+        await cardNumber.waitForDisplayed();
+        await cardNumber.setValue(1234567812345678);
+
+        const cardSignatureStrip = await $(page.cardSignatureStrip);
+        await cardSignatureStrip.waitForDisplayed();
+        await cardSignatureStrip.click();
+
+        const linkCardButton = await $(page.linkCardButton);
+        await linkCardButton.waitForDisplayed();
+        await linkCardButton.click();
+
+        const closePaymentMethodModalButton = await $(page.closePaymentMethodModalButton);
+        await closePaymentMethodModalButton.waitForDisplayed();
+        await closedPaymentMethodModalButton.click();
+        
         const cardPaymentMethodIcon = await $(page.cardPaymentMethodIcon);
         await cardPaymentMethodIcon.waitForDisplayed();
         await expect(await $(cardPaymentMethodIcon)).toBeExisting();
 
-        await browser.pause(2000);   
+        await browser.pause(2000);
+        
+    })   
 
-        //Input phone number
-        const phoneNumber = helper.getPhoneNumber("+1");
-        await page.submitPhoneNumber(phoneNumber);
-        await expect(await helper.getElementByText(phoneNumber)).toBeExisting);
+})        
+        
+           
+             
+              
+   
+        
+         
     
-     
-      
-})
 
+                   
+                      
+       
+             
+   
+        
+        
+        
+    
+   
+      
