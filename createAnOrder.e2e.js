d9e7e91c7254e6cd8d854fee1161087e71fd8028
Slie889 <suv99@live.com>
2025-02-22 21:47:05 -0800

    Add files via upload

---
+++ createAnOrder.e2e.js
@@ -0,0 +1,62 @@
+const page = require('../../page');
+const helper = require('../../helper')
+
+describe('Create an order', () => {
+    it('should wait for the taxi driver', async () => {
+        //Call the taxi to the address
+        await browser.url (`https://cnt-3fa17d70-09f2-4c8e-94cb-05511c73c312.containerhub.tripleten-services.com/`);
+        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
+
+       //Adding a payment card
+        const paymentMethodButton = await $(page.paymentMethodButton);
+        await paymentMethodButton.waitForDisplayed();
+        await paymentMethodButton.click();
+
+        const addCardButton = await $(page.addCardButton);
+        await addCardButton.waitForDisplayed();
+        await addCardButton.click();
+        const cardNumber = await $(page.cardNumber);
+        await cardNumber.waitForDisplayed();
+        await cardNumber.setValue(1234567812345678);
+
+        const cardSignatureStrip = await $(page.cardSignatureStrip);
+        await cardSignatureStrip.waitForDisplayed();
+        await cardSignatureStrip.click();
+
+        const linkCardButton = await $(page.linkCardButton);
+        await linkCardButton.waitForDisplayed();
+        await linkCardButton.click();
+
+        await browser.pause(2000);
+        
+    })    
+        
+})        
+        
+        
+
+        
+        
+    
+
+      
+        
+           
+             
+              
+   
+        
+         
+    
+
+                   
+                      
+       
+             
+   
+        
+        
+        
+    
+   
+      
