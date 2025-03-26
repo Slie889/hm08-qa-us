25117b89d5daf90ebd240932d781b97e4ac09c5f
Slie889 <suv99@live.com>
2025-03-25 22:39:30 -0700

    Update createAnOrder.e2e.js

--- createAnOrder.e2e.js
+++ createAnOrder.e2e.js
@@ -2,12 +2,51 @@ const page = require('../../page');
 const helper = require('../../helper')
 
 describe('Create an order', () => {
-    it('should wait for the taxi driver', async () => {
-        //Call the taxi to the address
-        await browser.url (`https://cnt-3fa17d70-09f2-4c8e-94cb-05511c73c312.containerhub.tripleten-services.com/`);
+    it('Should save the address', async () => {
+       //Setting the address
+       await browser.url ('/');
+       await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
+
+       await browser.pause(2000);
+    })    
+      
+    it('Selecting Supportive Plan', async () => {
+       //Setting the address
+       await browser.url ('/')
+       await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
+            
+       //Supportive Plan
+       const supportiveButton = await $(page.supportiveButton);
+       await supportiveButton.waitForDisplayed();
+       await supportiveButton.click();
+       await expect(await $(supportiveButton)).toBeExisting();
+
+       await browser.pause(4000);
+    })   
+
+    it('Send Message', async () => {
+        //Setting the address
+        await browser.url ('/');
         await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
 
-       //Adding a payment card
+        //Supportive Plan
+        const supportiveButton = await $(page.supportiveButton);
+        await supportiveButton.waitForDisplayed();
+        await supportiveButton.click();
+        
+        //Message to the driver
+        await page.FillMessage('Be on time');
+
+        await browser.pause(6000);
+    })
+     
+        
+    it('Adding a credit card', async () => {
+        //Setting the address
+        await browser.url ('/');
+        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
+        
+        //Adding a payment card
         const paymentMethodButton = await $(page.paymentMethodButton);
         await paymentMethodButton.waitForDisplayed();
         await paymentMethodButton.click();
@@ -15,9 +54,13 @@ describe('Create an order', () => {
         const addCardButton = await $(page.addCardButton);
         await addCardButton.waitForDisplayed();
         await addCardButton.click();
+
         const cardNumber = await $(page.cardNumber);
         await cardNumber.waitForDisplayed();
         await cardNumber.setValue(1234567812345678);
+        const cardCode = await $(page.cardCode);
+        await cardCode.waitForDisplayed();
+        await cardCode.setValue(12);
 
         const cardSignatureStrip = await $(page.cardSignatureStrip);
         await cardSignatureStrip.waitForDisplayed();
@@ -27,13 +70,19 @@ describe('Create an order', () => {
         await linkCardButton.waitForDisplayed();
         await linkCardButton.click();
 
-        await browser.pause(2000);
+        const closePaymentMethodModalButton = await $(page.closePaymentMethodModalButton);
+        await closePaymentMethodModalButton.waitForDisplayed();
+        await closePaymentMethodModalButton.click();
+
+        const cardPaymentMethodIcon = await $(page.cardPaymentMethodIcon);
+        await cardPaymentMethodIcon.waitForDisplayed();
+        await expect(await $(cardPaymentMethodIcon)).toBeExisting();
+
+        await browser.pause(10000);
+    })  
         
-    })    
-        
+      
 })        
-        
-        
 
         
         
